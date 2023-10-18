b8223cd790c1c3da64a2e0b9bf5dd2c2c872d8c5
UkReaksa <khsa598@gmail.com>
2023-10-18 17:17:43 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function gotoNextpage() {
+    window.location.href = "page.html";
+}
+
+function goBack() {
+    window.location.href = "index.html";
+}
+function login(){
+    window.location.href = "page.html";
+}
+function book1(){
+    window.location.href = "book1.html";
+}
+function book2(){
+    window.location.href = "book2.html";
+}
+function book3(){
+    window.location.href = "book3.html";
+}
+វ
+function book4(){
+    window.location.href = "book4.html";
+}
+function book5(){
+    window.location.href = "book5.html";
+}
+function book6(){
+    window.location.href = "book6.html";
+}
